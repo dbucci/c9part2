2e8446d29e205006a1efd402116e9ed5d8138ba2
David Bucci <davidjbucci@gmail.com>
2017-11-23 01:41:51 +0000

    css complete

---
+++ css.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+
+app.use(require('stylus').middleware(process.argv[3]||__dirname + '/public'))
+
+app.use(express.static(process.argv[3]||__dirname + '/public'))
+
+app.listen(process.argv[2])
